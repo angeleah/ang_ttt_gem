478ce5a56515e52015bd0d115f525ce248f97771
Kori Roys <kori@koriroys.com>
2012-09-08 15:57:20 -0500

    Message.passed cleaned up a little

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,15 +2,13 @@ require "spec_helper"
 require "message"
 
 describe "message" do
-  before do
-    @message = Message.new
-  end
+  let(:message) { Message.new }
 
   it "should be able to retrieve one message" do
-    @message.passed(:welcome).should == "Welcome to Tic Tac Toe! You will create 2 players. The first player you create will go first.\n"
+    message.passed(:welcome).should == "Welcome to Tic Tac Toe! You will create 2 players. The first player you create will go first.\n"
   end
 
-  it 'should be able to retrive multiple messages' do
-    @message.passed(:welcome,:create_player).should == "Welcome to Tic Tac Toe! You will create 2 players. The first player you create will go first.\n""Create a player.\n"
+  it 'should be able to retrieve multiple messages' do
+    message.passed(:welcome, :create_player).should == "Welcome to Tic Tac Toe! You will create 2 players. The first player you create will go first.\nCreate a player.\n"
   end
 end
